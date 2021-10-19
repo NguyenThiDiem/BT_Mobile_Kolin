088c55fdd4b1bf5dbe5e0ad5c3c665120753f77d
NguyenThiDiem <diem.nguyen22@student.passerellesnumeriques.org>
2021-10-19 13:43:58 +0700

    Adding code for assignment 1

--- JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
+++ JetNote/app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.jetnote
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.compose.setContent
+
+import com.example.jetnote.theme.JetNotesTheme
+import com.example.jetnote.ui.screens.NotesScreen
+
 import com.example.jetnote.viewmodel.MainViewModel
 import com.example.jetnote.viewmodel.MainViewModelFactory
 
@@ -12,18 +16,20 @@ import com.example.jetnote.viewmodel.MainViewModelFactory
  */
 class MainActivity : AppCompatActivity() {
 
-  private val viewModel: MainViewModel by viewModels(factoryProducer = {
-    MainViewModelFactory(
-      this,
-      (application as JetNotesApplication).dependencyInjector.repository
-    )
-  })
+    private val viewModel: MainViewModel by viewModels(factoryProducer = {
+        MainViewModelFactory(
+            this,
+            (application as JetNotesApplication).dependencyInjector.repository
+        )
+    })
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    setContent {
-
+        setContent {
+            JetNotesTheme {
+                NotesScreen(viewModel = viewModel)
+            }
+        }
     }
-  }
 }
---
+++ JetNote/app/src/main/java/com/example/jetnote/ui/screens/SaveNoteScreen.kt
@@ -0,0 +1,76 @@
+package com.example.jetnote.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetnote.domain.model.ColorModel
+import com.example.jetnote.routing.JetNotesRouter
+import com.example.jetnote.routing.Screen
+import com.example.jetnote.ui.components.AppDrawer
+import com.example.jetnote.ui.components.NoteColor
+import com.example.jetnote.ui.components.TopAppBar
+import com.example.jetnote.util.fromHex
+import com.example.jetnote.viewmodel.MainViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+private fun ColorPicker(
+    colors: List<ColorModel>,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Color picker",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            items(colors.size) { itemIndex ->
+                val color = colors[itemIndex]
+                ColorItem(
+                    color = color,
+                    onColorSelect = onColorSelect
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorItem(
+    color: ColorModel,
+    onColorSelect: (ColorModel) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { onColorSelect(color) })
+    ) {
+        NoteColor(
+            modifier = Modifier.padding(10.dp),
+            color = Color.fromHex(color.hex),
+            size = 80.dp,
+            border = 2.dp
+        )
+        Text(
+            text = color.name,
+            fontSize = 22.sp,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
